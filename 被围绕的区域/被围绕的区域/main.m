4b87629a8cda3e8fff1cc53a6e9ee5763ccc5e4f
zhilong.cheng <zhilong.cheng@yintech.cn>
2020-08-11 18:50:53 +0800

    no message

---
+++ 被围绕的区域/被围绕的区域/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  被围绕的区域
+//
+//  Created by lorne on 2020/8/11.
+//  Copyright © 2020 lorne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
