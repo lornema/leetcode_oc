97b02b110e0982ae0d79d103cb62713d5609ee25
zhilong.cheng <zhilong.cheng@yintech.cn>
2020-08-13 19:51:10 +0800

    no message

---
+++ 克隆图/克隆图/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  克隆图
+//
+//  Created by lorne on 2020/8/12.
+//  Copyright © 2020 lorne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
